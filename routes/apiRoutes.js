9c4b7dee9f1bb03bf67a31e946a13946b952438c
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-01 20:41:25 -0400

    got it to delete notes!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,26 +4,23 @@ const path = require('path');
 //get database 
 const notes = require('../Develop/db/db.json')
 const fs = require('fs');
-const createNewNote = require('../lib/notes');
+const { createNewNote, deleteNote } = require('../lib/notes');
+
+
 
   //GET /api/notes should read the db.json file and return all saved notes as JSON.
   router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../Develop/db/db.json'));
+    return res.sendFile(path.join(__dirname, '../Develop/db/db.json'));
 })
 
 //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 router.post('/notes', (req, res) => {
-  res.json(createNewNote(req.body));
+  return res.json(createNewNote(req.body));
 })
 
 //delete a note
 router.delete('/notes/:id', (req, res) => {
-  const id = req.params.id;
-    const newNotesData = notes.filter(el => 
-        el.id !== id
-    );
-    res.sendFile(path.join(__dirname, '../Develop/db/db.json'), JSON.stringify(newNotesData, null, 2));
-    console.log('hi');
+  return res.json(deleteNote(req.params.id));
 })
 
 module.exports = router;
